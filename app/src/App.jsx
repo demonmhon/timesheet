6c758bfc5cc0b577c552677a9f17c47302b96be3
demonmhon <thanawat.petchsri@gmail.com>
2024-06-02 23:16:14 +0700

    Init app

---
+++ app/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css'
+import './App.css'
+
+function App() {
+  return (
+    <MantineProvider><p>timesheet</p></MantineProvider>
+  )
+}
+
+export default App
