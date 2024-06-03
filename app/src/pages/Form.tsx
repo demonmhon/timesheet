4220722b387e0e5bfe3914b9728dd2476ed4d393
demonmhon <thanawat.petchsri@gmail.com>
2024-06-03 13:55:28 +0700

    Add form

--- app/src/pages/Form.tsx
+++ app/src/pages/Form.tsx
@@ -1,18 +1,27 @@
 import { useState } from 'react'
 import { DateInput } from '@mantine/dates'
 import { TimeInput } from '@mantine/dates'
+import { Button } from '@mantine/core'
+import { Box, Group } from '@mantine/core'
 
 export const Form = (props: any) => {
   const [value, setValue] = useState<Date | null>(null)
   return (
     <>
-      <DateInput
-        value={value}
-        onChange={setValue}
-        label="Date"
-        placeholder=""
-      />
-      <TimeInput label="Time" description="" placeholder="" />
+      <Box maw={340} mx="auto">
+        <Group mb="xs">
+          <DateInput
+            value={value}
+            onChange={setValue}
+            label="Date"
+            placeholder=""
+          />
+          <TimeInput label="Time" description="" placeholder="" />
+        </Group>
+        <Group mb="xs">
+          <Button variant="filled">Save</Button>
+        </Group>
+      </Box>
     </>
   )
 }
