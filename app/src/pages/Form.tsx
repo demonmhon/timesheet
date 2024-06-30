4b71336c86d80563b298c1c524982c0b8d3bf59a
demonmhon <thanawat.petchsri@gmail.com>
2024-06-30 22:59:59 +0700

    Add duration input

--- app/src/pages/Form.tsx
+++ app/src/pages/Form.tsx
@@ -1,22 +1,33 @@
 import { useState } from 'react'
 import { DateInput } from '@mantine/dates'
-import { TimeInput } from '@mantine/dates'
 import { Button } from '@mantine/core'
 import { Box, Group } from '@mantine/core'
+import { NumberInput } from '@mantine/core'
 
 export const Form = (props: any) => {
-  const [value, setValue] = useState<Date | null>(null)
+  const getDefaultDate = (): Date => new Date()
+  const [value, setValue] = useState<Date | null>(getDefaultDate())
   return (
     <>
       <Box maw={340} mx="auto">
         <Group mb="xs">
           <DateInput
+            size="xs"
             value={value}
             onChange={setValue}
             label="Date"
             placeholder=""
           />
-          <TimeInput label="Time" description="" placeholder="" />
+        </Group>
+        <Group mb="xs">
+          <NumberInput
+            size="xs"
+            label="Duration (hour)"
+            placeholder=""
+            decimalScale={1}
+            fixedDecimalScale
+            defaultValue={1}
+          />
         </Group>
         <Group mb="xs">
           <Button variant="filled">Save</Button>
