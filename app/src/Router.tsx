8348d6c7cc473f8edc78b0bac3356563e20286df
demonmhon <thanawat.petchsri@gmail.com>
2025-02-01 16:25:47 +0700

    Home page

--- app/src/Router.tsx
+++ app/src/Router.tsx
@@ -1,16 +1,17 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 import MainLayout from './layouts/Main'
-import Error from './pages/Error'
-import Timesheet from './pages/Timesheet'
+import ErrorPage from './pages/Error'
+import HomePage from './pages/Home'
+import TimesheetPage from './pages/Timesheet'
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route element={<MainLayout />}>
-        <Route index element={<Timesheet />} />
-        <Route path="/timesheet" element={<Timesheet />} />
-        <Route path="/404" element={<Error />} />
+        <Route index element={<HomePage />} />
+        <Route path="/timesheet" element={<TimesheetPage />} />
+        <Route path="/404" element={<ErrorPage />} />
         <Route path="*" element={<Navigate replace to="/404" />} />
       </Route>
     </Routes>
